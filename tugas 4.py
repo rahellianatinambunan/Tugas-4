c9cddb475d04d5ca0801ea4f4b364453d58aa844
rahellianatinambunan <116257323+rahellianatinambunan@users.noreply.github.com>
2022-11-19 15:28:15 +0700

    Add files via upload

---
+++ tugas 4.py
@@ -0,0 +1,27 @@
+a ="Me,-dan"
+c = a.split(",")
+print(c)
+
+j = "ka,-lim ,-an,-tan"
+d = j.split(",")
+print(d)
+
+m ='be,-ka,-si'
+n = m.split(",")
+print(n)
+
+s ='Ma,-na,-do'
+i =s.split('/')
+print(i)
+
+k = 'be|ka|si'
+o = k.split('|')
+print(o)
+
+h = 'ja+mbi'
+f = h.split('+')
+print (f)
+
+g = "ba*nd*dung"
+u = g.split("*")
+print(u)
